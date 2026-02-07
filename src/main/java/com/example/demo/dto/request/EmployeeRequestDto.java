2d4f5eae8735b3003255a46002d26eefd818799c
Samir Alaa <76245712+samiralaa@users.noreply.github.com>
2026-02-07 21:00:50 +0400

    Update Java version to 21

--- src/main/java/com/example/demo/dto/request/EmployeeRequestDto.java
+++ src/main/java/com/example/demo/dto/request/EmployeeRequestDto.java
@@ -1,19 +1,21 @@
 package com.example.demo.dto.request;
 
-import org.antlr.v4.runtime.misc.NotNull;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.Email;
+import jakarta.validation.constraints.NotNull;
+import jakarta.validation.constraints.Positive;
 
 public class EmployeeRequestDto {
 
     @NotBlank(message = "Employee name is required")
     private String name;
 
-    @Email (message = "Invalid email format")
-    @NotBlank(message  = " Email is required")
+    @Email(message = "Invalid email format")
+    @NotBlank(message = "Email is required")
     private String email;
 
-    
-    @NotNull(message ="salary is requried")
-    @Positive(message = "salary is most be positive")
+    @NotNull(message = "salary is required")
+    @Positive(message = "salary must be positive")
     private double salary;
 
 
